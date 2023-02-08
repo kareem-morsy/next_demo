7c7a07fc34519511000581d40b8e840c76160a36
karem-morsy <kareem.morsy2014@gmail.com>
2023-02-08 17:07:04 +0200

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import '../styles/globals.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+// import '../node_modules/bootstrap/dist/js/bootstrap.min.js'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
